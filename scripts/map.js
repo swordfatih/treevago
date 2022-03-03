0835c8de2449c13f4ec6cd33c0a9a8d359b65420
swordfatih <accfldekur@gmail.com>
2022-03-03 14:40:08 +0100

    url

--- scripts/map.js
+++ scripts/map.js
@@ -2,7 +2,15 @@
 
 
 window.onload = function () {
-	
+    $( function() {
+        $( ".widget input[type=submit], .widget a, .widget button" ).button();
+        $( "button, input, a" ).click( function( event ) {
+          event.preventDefault();
+        } );
+      } );
+}
+
+/*
 	//Chargement initial de la MAP
 	var map = L.map('map').setView([14,-14.8883335],4);
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'PING'}).addTo(map);
@@ -88,9 +96,7 @@ window.onload = function () {
 		    }
 		});
 	}
-	
-	
 }
-
+*/
 
 
