d3f9b5b995c8f92773583f5f4b3439211340793d
swordfatih <accfldekur@gmail.com>
2022-03-03 15:21:19 +0100

    ajax

--- scripts/map.js
+++ scripts/map.js
@@ -21,16 +21,20 @@ window.onload = function() {
                     console.log(nom + " " + coordinates);
                 });
             }).catch((erreur) => {
-                alert("tes moche");
+                alert("tes moche " + erreur);
             });
         });
     });
+
+    //Chargement initial de la MAP
+	let map = L.map('map').setView([14,-14.8883335], 4);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'PING'}).addTo(map);
+
+    
 }
 
 /*
-	//Chargement initial de la MAP
-	var map = L.map('map').setView([14,-14.8883335],4);
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'PING'}).addTo(map);
+	
 	
 	//Rendre draggable les div des pays
 	$( "#France" ).draggable({ revert: "valid" });
